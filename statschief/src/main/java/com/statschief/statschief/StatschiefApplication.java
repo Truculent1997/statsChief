48301b1f563cba2b26f7bfd33c965898716e2b3c
Sagar Sharma <sagartruculent2@gmail.com>
2022-01-05 00:55:43 +0530

    Initial Commit

---
+++ statschief/src/main/java/com/statschief/statschief/StatschiefApplication.java
@@ -0,0 +1,13 @@
+package com.statschief.statschief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatschiefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatschiefApplication.class, args);
+	}
+
+}
---
+++ statschief/src/test/java/com/statschief/statschief/StatschiefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statschief.statschief;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatschiefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
